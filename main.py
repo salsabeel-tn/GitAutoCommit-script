8f136316c34f50ae949b8f5a1f28654d8c78a519
Salsabeel-tn <salsabeelj66@gmail.com>
2022-03-21 13:03:13 +0300

    autocommit

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
 import os
 import sys
 commitName = "autocommit"
-projectFolder = "C:\\Users\\salsa\\Desktop\\project4\\test_project\\"
+currentFolder = __file__[:-7]
+os.chdir(currentFolder)
 
 def updateDates():
-    f = open(projectFolder + "fileDates.txt", "w")
-    for file in os.listdir(projectFolder):
+    f = open(currentFolder + "fileDates.txt", "w")
+    for file in os.listdir(currentFolder):
         if (file != "main.py" and file != ".git" and file != "fileDates.txt"):
-            f.write(file + "=" + str(os.stat(projectFolder + file).st_mtime) + "\n")
+            f.write(file + "=" + str(os.stat(currentFolder + file).st_mtime) + "\n")
     f.close()
 
 def getsavedMDate(fileName):
-    f = open(projectFolder + "fileDates.txt", "r")
+    f = open(currentFolder + "fileDates.txt", "r")
     for line in f:
         if (len(line) > 0 and line.split("=")[0] == fileName):
             return line.split("=")[1]
 
 def getcurrentMDate(fileName):
-    for file in os.listdir(projectFolder):
+    for file in os.listdir(currentFolder):
         if (file == fileName):
-            return os.stat(projectFolder + file).st_mtime
+            return os.stat(currentFolder + file).st_mtime
 
-currentFolder = __file__[:-7]
-
-os.chdir(currentFolder)
-os.system("git add --all")
-os.system("git commit -m " + commitName)
-os.system("git push origin master")
-
-# while True:
-#     os.system("cd " + projectFolder)
-#     for file in os.listdir(projectFolder):
-#         if not file in ["main.py", ".git", "fileDates.txt", "tempCodeRunnerFile.py"]:
-#             if (float(getcurrentMDate(file)) > float(getsavedMDate(file))):
-#                 os.system("git add --all")
-#                 updateDates()
-#     os.system("git commit -m " + commitName)
-#     # os.system("git push origin master")
+while True:
+    os.system("cd " + currentFolder)
+    for file in os.listdir(currentFolder):
+        if not file in ["main.py", ".git", "fileDates.txt", "tempCodeRunnerFile.py"]:
+            if (float(getcurrentMDate(file)) > float(getsavedMDate(file))):
+                os.system("git add --all")
+                updateDates()
+    os.system("git commit -m " + commitName)
+    os.system("git push origin master")
